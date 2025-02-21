783e77288b273c7172939fd606581d97890828cc
Cecchy2 <dariocecchinato@gmail.com>
2025-02-21 09:39:05 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import MainPage from "./components/MainPage";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,12 @@
+import MyNavBar from "./MyNavBar";
+
+const MainPage = () => {
+  return (
+    <div className="MainPage">
+      <MyNavBar />
+      <h1>Main Page</h1>
+    </div>
+  );
+};
+
+export default MainPage;
