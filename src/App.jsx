f21fb4b6142b2e7667c4af2157d8f9adc7e5cc0b
Cecchy2 <dariocecchinato@gmail.com>
2025-02-21 10:10:55 +0100

    aggiunto font

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import "@fontsource/metropolis";
+import "@fontsource/metropolis/400.css";
+import "@fontsource/metropolis/500.css";
+import "@fontsource/metropolis/600.css";
+import "@fontsource/metropolis/700.css";
+import "@fontsource/metropolis/800.css";
+import "@fontsource/metropolis/900.css";
 
 import MainPage from "./components/MainPage";
 
--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -1,33 +1,34 @@
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 
 const MyNavBar = () => {
   return (
-    <Navbar expand="lg" className="bg-transparent">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src="/public/Baietta Web/BAIETTAWHITE.png"
-            width="60"
-            height="60"
-            className="d-inline-block align-top"
-          />{" "}
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar expand="lg" className="bg-transparent myNavBar">
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src="/public/Baietta Web/BAIETTAWHITE.png"
+          width="60"
+          height="60"
+          className="d-inline-block align-top ms-5"
+        />{" "}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto ms-4 border-start border-1 border-white">
+          <Nav.Link href="#home" className="myNavLink ms-4">
+            HOME
+          </Nav.Link>
+          <Nav.Link href="#link" className="myNavLink">
+            CHI SIAMO
+          </Nav.Link>
+          <Nav.Link href="#link" className="myNavLink">
+            CORSI
+          </Nav.Link>
+          <Nav.Link href="#link" className="myNavLink">
+            CONTATTI
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
