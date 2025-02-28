d42e1d85599ab403dc2a9eb1acef6d640ddeb011
Cecchy2 <dariocecchinato@gmail.com>
2025-02-28 16:08:52 +0100

    terminato footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import { Col, Container, Image, Row } from "react-bootstrap";
+import { Col, Container, Image, Nav, Row } from "react-bootstrap";
+import { IoLogoFacebook, IoLogoInstagram } from "react-icons/io";
 
 const Footer = () => {
   return (
@@ -6,22 +7,26 @@ const Footer = () => {
       <div className="footer"></div>
       <div className="underFooter">
         <Container>
-          <Row>
+          <Row className="justify-content-center">
             {" "}
-            <Col md={3} className="text-light">
-              <Image src="/public/Baietta Web/BAIETTAWHITE-300x300.png" className="footerLogo" />
-            </Col>
-            <Col className="text-light align-item-flex-start" md={3}>
+            <Col className="text-light align-item-flex-start" md={5}>
               <h3 className="">DOVE SIAMO</h3>
               <p>Contrada Grassuri, 90047 Partinico (PA)</p>
             </Col>
-            <Col className="text-light align-item-flex-start" md={3}>
+            <Col className="text-light align-item-flex-start" md={4}>
               <h3>CONTATTI</h3>
               <p>Info@labaietta.it</p>
+              <Image src="/public/Baietta Web/BAIETTAWHITE-300x300.png " className="footerLogo" />
             </Col>
             <Col className="text-light align-item-flex-start" md={3}>
-              <a href="">Privacy Policy</a>
+              <Nav.Link href="">
+                <h3>Links Utili</h3>
+              </Nav.Link>
+              <Nav.Link href="">Privacy Policy</Nav.Link>
+              <IoLogoInstagram size={40} />
+              <IoLogoFacebook size={40} className="ms-4" />
             </Col>
+            <p className="text-light">Club Nautico La Baietta A.R.L. All Rights Reserved C.F. 97129040826</p>
           </Row>
         </Container>
       </div>
