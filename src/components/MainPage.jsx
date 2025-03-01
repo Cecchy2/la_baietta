3cc0a7d527e5da1321a6db797089f89bb0166de7
Cecchy2 <dariocecchinato@gmail.com>
2025-03-01 11:46:47 +0100

    aggiunto logo fisww

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -11,6 +11,8 @@ import GoogleMapComponent from "./GoogleMapComponent";
 import SectionReview from "./SectionReview";
 import FormMessage from "./FormMessage";
 import Footer from "./Footer";
+import { IoLogoInstagram } from "react-icons/io";
+import { AiOutlineFacebook } from "react-icons/ai";
 
 const MainPage = () => {
   return (
@@ -23,6 +25,13 @@ const MainPage = () => {
         <div className="centerContainer">
           <Image src="/Baietta Web/BAIETTAWHITE.png" className="baiettaLogo" />
           <Button className="btnPrenota">PRENOTA ORA</Button>
+        </div>
+        <div className="socialIcons">
+          <AiOutlineFacebook size={50} />
+          <IoLogoInstagram size={50} />
+        </div>
+        <div>
+          <Image src="/public/Baietta Web/Loghi/logo-fissw2.png" className="fiswwLogo" />
         </div>
       </div>
       <SectionWave />
